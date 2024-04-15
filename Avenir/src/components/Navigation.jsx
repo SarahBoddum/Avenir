bb7f863b94a7460a880629b166d2410c9f55d932
SarahBoddum <126453046+SarahBoddum@users.noreply.github.com>
2024-04-15 09:24:33 +0200

    Al burger begyndelse er svær

--- Avenir/src/components/Navigation.jsx
+++ Avenir/src/components/Navigation.jsx
@@ -3,9 +3,12 @@ import { NavLink } from "react-router-dom";
 export default function Navigation() {
     return (
         <nav className="navigation">
-            <NavLink to="/">Forside</NavLink>
-            <NavLink to="/Om">Om os</NavLink>
-            <NavLink id="1234" className={'hej'} to="/contact">Kontakt</NavLink>
+            <NavLink className="navlink" id="logo" to="/">AVENIR</NavLink>
+            <NavLink className="navlink" to="/Om">OM OS</NavLink>
+            <NavLink id="1234" className="navlink" to="/contact">SERVICES</NavLink>
+            <NavLink className="navlink"to="/Om">CASES</NavLink>
+            <NavLink className="navlink"to="/Om">BLOG</NavLink>
+            <NavLink className="navlink" id="kontakt" to="/Om">KONTAKT</NavLink>
         </nav>
     )
 }
